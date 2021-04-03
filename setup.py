32abc8dbc9274e0a43fbdc118017a49109638556
OliverEros <olivereros@protonmail.com>
2021-04-03 10:22:54 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
         author = 'Oliver Eros',
         description = 'Automation tool for deleting images in HTML emails',
         packages = find_packages(),
+        python_requires='>=3',
+        install_requires = [
+                            'colorama'
+                           ]
         entry_points = { 'console_scripts' : ['picsdump=main.picsdump:start']},
         )
